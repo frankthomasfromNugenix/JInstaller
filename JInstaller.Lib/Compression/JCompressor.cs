951b684dad2ad10621770d206414a790a3246e74
frankthomasfromNugenix <75461640+frankthomasfromNugenix@users.noreply.github.com>
2022-04-25 16:49:38 -0600

    init

---
+++ JInstaller.Lib/Compression/JCompressor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JInstaller.Compression
+{
+    public class JCompressor
+    {
+        public Task Compress(byte[] byteArray)
+        {
+            MemoryStream stream = new MemoryStream();
+
+        }
+    }
+}
